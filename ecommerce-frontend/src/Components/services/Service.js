0885a3fe3c723c5790186d2787ac6f30571c204b
unknown <ssgsj.carranza@gmail.com>
2021-05-18 13:22:51 -0500

    created axios call to login user

--- ecommerce-frontend/src/Components/services/Service.js
+++ ecommerce-frontend/src/Components/services/Service.js
@@ -16,3 +16,14 @@ export async function registerService(user){
     }
 }
 
+export async function loginService(login){
+    try {
+        const response = await axios.post(apiEndpoint + '/authentication/login', login);
+        console.log("You are logged in");
+        return response.data;
+    }
+    catch(ex){
+        console.log("Error");
+        throw ex;
+    }
+}

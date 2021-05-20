cd2c50571ebdc49aa271fc1034661c7957c7e05f
unknown <ssgsj.carranza@gmail.com>
2021-05-20 12:56:43 -0500

    pulling merger

--- ecommerce-frontend/src/index.js
+++ ecommerce-frontend/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import reducer, {initialState} from './Components/reducer/Reducer';
+import StateProvider from './Components/contextAPI/StateProvider';
 
+//enclosed App in StateProvider so children get access to state too. By passing initialState and reducer, the state know what is inital and reducer
 ReactDOM.render(
   <Router>
     <React.StrictMode>
+      <StateProvider initialState={initialState} reducer={reducer}>
       <App />
+    </StateProvider>
     </React.StrictMode>
   </Router>,
   document.getElementById('root')
